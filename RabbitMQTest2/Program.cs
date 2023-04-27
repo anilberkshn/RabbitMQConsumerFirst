62cc1a4fb800500ca93cf97116f49f5d127622f2
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2023-04-27 19:13:01 +0300

    2. yapılan çalıştı.

---
+++ RabbitMQTest2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost", // RabbitMQ'nun bulunduğu sunucu adresi
+                // Port = 5672, // RabbitMQ'nun kullanacağı bağlantı noktası
+                VirtualHost = "/" // RabbitMQ sanal ana bilgisayar (virtual host)
+            };
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+        
+            // Exchange ve Queue oluşturma
+            channel.ExchangeDeclare("my_exchange", ExchangeType.Direct);
+            channel.QueueDeclare("my_queue", false, false, false, null);
+            channel.QueueBind("my_queue", "my_exchange", "my_routing_key");
+
+            // Mesajı gönderme
+            var message = "Hello, RabbitMQ!";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("my_exchange", "my_routing_key", null, body);
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
