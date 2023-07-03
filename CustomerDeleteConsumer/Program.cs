8f94f3931161ce259ee2850db2b65faa2b73382d
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2023-07-03 19:35:11 +0300

    CustomerDeleteConsumer.csproj

---
+++ CustomerDeleteConsumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace CustomerDeleteConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare("CustomerDelete",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            
+            Console.WriteLine(" Waiting for messages.");
+            
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" Received : {message}");
+                
+            };
+            channel.BasicConsume(queue: "CustomerDelete",
+                autoAck: true,
+                consumer: consumer);
+            
+            Console.WriteLine(" Press [enter] to exit."); 
+            Console.ReadLine();
+
+        }
+    }
+}
--- RabbitMQConsumer2/Program.cs
+++ RabbitMQConsumer2/Program.cs
@@ -9,30 +9,30 @@ namespace RabbitMQConsumer2
     {
         static void Main(string[] args)
         {
-            var connectionFactory = new ConnectionFactory
-            {
-                HostName = "localhost", // RabbitMQ'nun bulunduğu sunucu adresi
-                Port = 5672, // RabbitMQ'nun kullanacağı bağlantı noktası
-                VirtualHost = "/" // RabbitMQ sanal ana bilgisayar (virtual host)
-            };
-            
-            var connection = connectionFactory.CreateConnection();
-            var channel = connection.CreateModel();
-            
-            // Queue oluşturma
-            channel.QueueDeclare("my_queue", false, false, false, null);
-
-// Mesajları alma
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (model, ea) =>
-            {
-                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
-                Console.WriteLine("Received message: {0}", message);
-            };
-            channel.BasicConsume("my_queue", true, consumer);
-
-            channel.Close();
-            connection.Close();
+//             var connectionFactory = new ConnectionFactory
+//             {
+//                 HostName = "localhost", // RabbitMQ'nun bulunduğu sunucu adresi
+//                 Port = 5672, // RabbitMQ'nun kullanacağı bağlantı noktası
+//                 VirtualHost = "/" // RabbitMQ sanal ana bilgisayar (virtual host)
+//             };
+//             
+//             var connection = connectionFactory.CreateConnection();
+//             var channel = connection.CreateModel();
+//             
+//             // Queue oluşturma
+//             channel.QueueDeclare("my_queue", false, false, false, null);
+//
+// // Mesajları alma
+//             var consumer = new EventingBasicConsumer(channel);
+//             consumer.Received += (model, ea) =>
+//             {
+//                 var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+//                 Console.WriteLine("Received message: {0}", message);
+//             };
+//             channel.BasicConsume("my_queue", true, consumer);
+//
+//             channel.Close();
+//             connection.Close();
 
         }
     }
