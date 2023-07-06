038838196333396f4e146c8da31bd83b24c777bc
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2023-07-06 15:17:34 +0300

    Çalışmış hali

--- CustomerDeleteConsumer/Program.cs
+++ CustomerDeleteConsumer/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -25,8 +28,30 @@ namespace CustomerDeleteConsumer
             consumer.Received += (model, ea) =>
             {
                 var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                Console.WriteLine($" Received : {message}");
+                // var bodyToGuid = new Guid(ea.Body.ToArray()                                                                                                                                                                                                                                            );
+                var message = System.Text.Encoding.UTF8.GetString(body);
+                var message1 = message.Trim('"');
+                Console.WriteLine($" Received : {message1}");
+                var guidParse = Guid.Parse(message1);
+                using (var httpClient = new HttpClient())
+                {
+                    var apiUrl = $"http://localhost:5011/api/orders/Customer/{message1}";
+                    //string apiUrl = $"http://localhost:5011/api/orders/Customer/20636b1d-a1bc-4581-b01f-3f066ff8f474"; // çalışıyor.      
+                    var content = new StringContent(message1,Encoding.UTF8,"application/json");
+                    var response = httpClient.PostAsync(apiUrl,content).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("API isteği başarılı. Yanıt içeriği:");
+                        var responseContent = response.Content.ReadAsStringAsync().Result;
+                        Console.WriteLine(responseContent);
+                    }
+                    else
+                    {
+                        Console.WriteLine("API isteği başarısız. Hata kodu: " + response.StatusCode);
+                    }
+                }
+                
                 
             };
             channel.BasicConsume(queue: "CustomerDelete",
